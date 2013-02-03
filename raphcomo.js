4baf7ac60d99a73c5298072d1bb6db7d61a417d2
trokster <blucountry@yahoo.com>
2013-02-03 20:41:54 +0100

    updated raphcomo with a couple wrapping algs

--- raphcomo.js
+++ raphcomo.js
@@ -27,36 +27,79 @@ window.wordwrap = function(text, lineWidth) {
     }
 };
 
+//+ Jonas Raoni Soares Silva
+//@ http://jsfromhell.com/string/wordwrap [rev. #2]
+
+String.prototype.wordWrap = function(m, b, c){
+    var i, j, l, s, r;
+    if(m < 1)
+        return this;
+    for(i = -1, l = (r = this.split("\n")).length; ++i < l; r[i] += s)
+        for(s = r[i], r[i] = ""; s.length > m; r[i] += s.slice(0, j) + ((s = s.slice(j)).length ? b : ""))
+            j = c == 2 || (j = s.slice(0, m + 1).match(/\S*(\s)?$/))[1] ? m : j.input.length - j[0].length
+            || c == 1 && m || j.input.length + (j = s.slice(m).match(/^\S*/)).input.length;
+    return r.join("\n");
+};
+
+window.rwrap = function(t, width) {
+    var content = t.attr("text");
+    var abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    t.attr({
+      "text" : abc
+    });
+    var letterWidth = t.getBBox().width / abc.length;
+
+    t.attr({
+        "text" : content
+    });
+
+    //+ Jonas Raoni Soares Silva
+    //@ http://jsfromhell.com/string/wordwrap [rev. #2]
+    var i, j, l, s, r, m, b, c, content;
+    m=Math.round(width/letterWidth);
+    b = "\n";
+    c = 1;
+
+    for(i = -1, l = (r = content.split("\n")).length; ++i < l; r[i] += s)
+        for(s = r[i], r[i] = ""; s.length > m; r[i] += s.slice(0, j) + ((s = s.slice(j)).length ? b : ""))
+            j = c == 2 || (j = s.slice(0, m + 1).match(/\S*(\s)?$/))[1] ? m : j.input.length - j[0].length
+            || c == 1 && m || j.input.length + (j = s.slice(m).match(/^\S*/)).input.length;
+    
+
+    t.attr({
+        "text" : r.join("\n").replace("\n ", "\n")
+    });
+}
+
 var DumpObject = function(obj) {
-        var od = new Object;
-        var result = "";
-        var len = 0;
+    var od = new Object;
+    var result = "";
+    var len = 0;
 
-        for(var property in obj) {
-            var value = obj[property];
-            if(typeof value == 'string') value = "'" + value + "'";
-            else if(typeof value == 'object') {
-                if(value instanceof Array) {
-                    value = "[ " + value + " ]";
-                } else {
-                    var ood = DumpObject(value);
-                    value = "{ " + ood.dump + " }";
-                }
+    for(var property in obj) {
+        var value = obj[property];
+        if(typeof value == 'string') value = "'" + value + "'";
+        else if(typeof value == 'object') {
+            if(value instanceof Array) {
+                value = "[ " + value + " ]";
+            } else {
+                var ood = DumpObject(value);
+                value = "{ " + ood.dump + " }";
             }
-            result += "'" + property + "' : " + value + ", ";
-            len++;
         }
-        od.dump = result.replace(/, $/, "");
-        od.len = len;
-
-        return od;
+        result += "'" + property + "' : " + value + ", ";
+        len++;
     }
+    od.dump = result.replace(/, $/, "");
+    od.len = len;
 
-    //Force use of indexedDB and 
-    //window.shimIndexedDB && window.shimIndexedDB.__useShim();
-    //Register current size
-    window.current_dimensions = {};
-//And we need binary translators
+    return od;
+}
+
+//Force use of indexedDB and 
+//window.shimIndexedDB && window.shimIndexedDB.__useShim();
+//Register current size
+window.current_dimensions = {}; //And we need binary translators
 if(!window.btoa) window.btoa = base64.encode
 if(!window.atob) window.atob = base64.decode
 
@@ -190,7 +233,7 @@ reviveJSON = function(o) {
         //Override Mochikit Base, parenthesis dont work for functions
         return eval("[" + MochiKit.Base._filterJSON(o) + "][0]");
     }
-    if(objtype == "string" || objtype == "number") {
+    if(objtype == "string" || objtype == "number" || objtype == "boolean") {
         return o;
     }
     // recurse
@@ -274,13 +317,15 @@ var waitForEvents = function(events) {
 var progressBar = function() {
         var res = {
             'init': function() {
+                this.log_length = window.INITIAL_LOG_LENGTH || 5;
                 //Set Component Relative Dimensions
-                this.width_pct = 50 / 100;
+                var width_pct = this.width_pct = 70 / 100;
                 this.height_pct = 45 / 100;
-                var percentage = this.percentage = 0.1;
+                var percentage = this.percentage = 0.01;
+                var fontsize = this.fontsize = 9;
                 this.border = 1;
                 this.height = 100;
-                this.width = 250;
+                this.width = (this.fontsize + 2) * 30;
                 //Define self for future reference
                 var self = this;
                 //Create DIV container
@@ -302,27 +347,40 @@ var progressBar = function() {
                     fill: "30-#191D24-#4B6275",
                     "stroke": "#191D24",
                     "fill-opacity": 1,
-                    "stroke-opacity": 1,
-                    "stroke-width": 2
+                    "stroke-opacity": 0,
+                    "stroke-width": this.border,
+                    blur: Raphael.vml ? 0 : 0
                 });
-                var outer_rect = this.outer_rect = paper.rect(0,0,50,50,5);
-                var inner_rect = this.inner_rect = paper.rect(0,0,50,50,5);
+                var outer_rect = this.outer_rect = paper.rect(0, 0, 50, 50, 5);
+                var inner_rect = this.inner_rect = paper.rect(0, 0, 50, 50, 5);
                 inner_rect.toFront();
                 outer_rect.attr({
                     fill: "#466B7A",
                     "stroke": "#172B33",
                     "fill-opacity": 1,
-                    "stroke-opacity": 1,
+                    "stroke-opacity": 0,
                     "stroke-width": this.border
                 });
                 inner_rect.attr({
-                    fill: "30-#8BD442-#E9FFD4",
-                    "stroke": "#191D24",
+                    fill: "30-#73B500-#E9FFD4",
+                    "stroke": "#73B500",
                     "fill-opacity": 1,
                     "stroke-opacity": 1,
-                    "stroke-width": 1,
-                    blur:Raphael.vml ? 0 : 1
+                    "stroke-width": this.border,
+                    blur: Raphael.vml ? 0 : 0
                 });
+                var text = this.text = paper.text(0, 0, "Init...");
+                text.attr({
+                    "fill": "white",
+                    "stroke-width": .5,
+                    "stroke": "white",
+                    "stroke-opacity": .2,
+                    "font-size": this.fontsize,
+                    //"font-weight": "bold",
+                    "font-family": "Arial, Helvetica, sans-serif",
+                    "fill-opacity": .8
+                });
+
                 this.resize = bind(function() {
                     var d = getViewportDimensions();
                     var width = this.width;
@@ -332,25 +390,29 @@ var progressBar = function() {
                     });
                     setElementDimensions(this.container, {
                         w: width + this.border * 2,
-                        h: this.height + this.border * 2
+                        h: this.height + this.border * 2 + +this.log_length * (this.fontsize + 2)
                     });
                     this.bgrect.attr({
-                        "width": width - 1,
-                        "height": this.height
+                        "width": width - this.border,
+                        "height": this.height + this.log_length * (this.fontsize + 2)
                     });
+                    this.paper.setSize(width + this.border * 2, this.height + this.border * 2 + this.log_length * (this.fontsize + 2));
                     this.outer_rect.attr({
-                        "width":width/2,
-                        "height":this.height/5,
-                        "x": width/4,
-                        "y": this.height/2-this.height/5/2
+                        "width": width * width_pct,
+                        "height": this.height / 6 + this.border,
+                        "x": width * (1 - width_pct) / 2,
+                        "y": this.height / 2 - this.height / 7 / 2
                     });
                     this.inner_rect.attr({
-                        "width": (width/2) * this.percentage+5,
-                        "height":this.height/5,
-                        "x": width/4,
-                        "y": this.height/2-this.height/5/2
+                        "width": width * width_pct * this.percentage,
+                        "height": this.height / 6,
+                        "x": width * (1 - width_pct) / 2,
+                        "y": this.height / 2 - this.height / 7 / 2
                     });
-                    this.paper.setSize(width + this.border * 2, this.height + this.border * 2);
+                    this.text.attr({
+                        "x": width / 2,
+                        "y": this.height * .7 + (this.log_length * (this.fontsize + 2)) / 2
+                    });
                 }, this);
                 //Attach resize method to window resize
                 eve.on("window.resize", this.resize);
@@ -361,41 +423,58 @@ var progressBar = function() {
                     paper.remove();
                     removeElement(self.container);
                 }
-                this.progress = function(){
+                this.progress = function() {
                     //log2("In progress: " + JSON.stringify(this));
                     var max = this[self.guidelines["max"]];
                     var measure = this[self.guidelines["x"]];
+                    var remaining = this[self.guidelines["remaining"]] || [];
+
                     //log2("Results: " + [measure, max, max == 0 ? 0 : measure / max])
-
                     self.percentage = max == 0 ? 0 : measure / max;
-                    if(self.percentage ==0) self.percentage = 0.1;
+                    if(self.percentage == 0) self.percentage = 0.01;
                     self.inner_rect.stop();
-                    self.inner_rect.animate({"width":(self.width/2 -2) * self.percentage}, 500, "bounce");
-
+                    self.inner_rect.animate({
+                        "width": self.width * self.width_pct * self.percentage
+                    }, 100, "smooth");
+                    var to_display = [];
+                    for(var i = 0; i < (self.log_length - 1); i++) {
+                        to_display.push(remaining.length > i ? (i == 0 ? "" : "\n") + remaining[i] : "");
+                    }
+                    to_display.push(i > self.log_length ? "\n..." : "");
+                    self.text.attr({
+                        "text": "Replicating: " + remaining.length + "\n" + reduce(function(a, b) {
+                            return a + b;
+                        }, to_display)
+                    });
                 };
-                this.listenTo = function(evt, guidelines){
+                this.listenTo = function(evt, guidelines) {
                     self.guidelines = guidelines;
                     eve.on(evt, self.progress);
                     self.evt = evt;
                 };
                 this.resize();
                 var cls = "progressBar";
-                req = {id:this.name};
+                req = {
+                    id: this.name
+                };
                 var o = res;
-                var reqh = function(){
-                    eve("interface.response_handle."+ cls + "." +req.id, o); 
-                };
-                eve.on("interface.request_handle."+cls+"." + req.id, reqh);
-                eve("interface.response."+cls+"."+req.id, {"req": req, "obj": o});
-                
-                var dest = function(){
-                    eve.off("interface."+cls+"."+req.id+".*");
-                    eve.off("interface.request_handle."+cls+"." + req.id);
-                    if(!isUndefinedOrNull(o.cleanup)) o.cleanup();
-                    //Callback if defined ( Handy in case you need the object to re-instantiate )
-                    if(!isUndefinedOrNull(this.callback)) this.callback();
-                };
-                eve.once("interface.remove."+cls+"."+req.id, dest);
+                var reqh = function() {
+                        eve("interface.response_handle." + cls + "." + req.id, o);
+                    };
+                eve.on("interface.request_handle." + cls + "." + req.id, reqh);
+                eve("interface.response." + cls + "." + req.id, {
+                    "req": req,
+                    "obj": o
+                });
+
+                var dest = function() {
+                        eve.off("interface." + cls + "." + req.id + ".*");
+                        eve.off("interface.request_handle." + cls + "." + req.id);
+                        if(!isUndefinedOrNull(o.cleanup)) o.cleanup();
+                        //Callback if defined ( Handy in case you need the object to re-instantiate )
+                        if(!isUndefinedOrNull(this.callback)) this.callback();
+                    };
+                eve.once("interface.remove." + cls + "." + req.id, dest);
             },
             '_id': 'progressBar'
         };
